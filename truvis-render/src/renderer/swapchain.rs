3d338265d75f88baa816883c164f50d687218ce7
qizhengjie <qzj@163.com>
2025-05-30 11:13:36 +0800

    fix swapchain image 支持 storage usage

--- truvis-render/src/renderer/swapchain.rs
+++ truvis-render/src/renderer/swapchain.rs
@@ -178,7 +178,11 @@ impl RhiSwapchain {
             .image_extent(extent)
             .image_array_layers(1)
             // TRANSFER_DST 用于 Nsight 分析
-            .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT | vk::ImageUsageFlags::TRANSFER_DST)
+            .image_usage(
+                vk::ImageUsageFlags::COLOR_ATTACHMENT
+                    | vk::ImageUsageFlags::TRANSFER_DST
+                    | vk::ImageUsageFlags::STORAGE,
+            )
             .pre_transform(surface_capabilities.current_transform)
             .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
             .present_mode(present_mode)
